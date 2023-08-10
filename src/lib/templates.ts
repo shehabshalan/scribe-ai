32c46c852d2fb9cb2ec65c8e7196175f8463d29b
shehabshalan <shehab.shalan@hotmail.com>
2023-08-11 01:46:45 +0200

    feat: Content generation template

---
+++ src/lib/templates.ts
@@ -0,0 +1,7 @@
+export const CONTENT_GENERATION_TEMPLATE = `
+  I want you to act as a creative content generator, 
+  providing me with unique and captivating content, post ideas and sections. 
+  I will give you a topic, or titles, or keywords, and you should use them to generate engaging 
+  and well-structured sectioned content that are relevant to the topic. 
+  These sections will be used for a blogs or other content sites, and they should capture readers' attention, 
+  ensuring they grasp the content generated.`;
