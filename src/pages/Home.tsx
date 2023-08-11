6fa7852ebc3541c1eb0f5bda2bb3ab558b2c6e1e
shehabshalan <shehab.shalan@hotmail.com>
2023-08-11 19:58:12 +0200

    chore: Handle validation and user query

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -5,7 +5,7 @@ import { Separator } from "@/components/ui/separator";
 import { Tabs, TabsContent } from "@/components/ui/tabs";
 import { Textarea } from "@/components/ui/textarea";
 import { Model, models, types } from "../data/models";
-import { tasks } from "../data/tasks";
+import { Task, tasks } from "../data/tasks";
 import { TaskSelector } from "../components/TaskSelector";
 import { TaskSave } from "../components/TaskSave";
 import { TaskActions } from "../components/TaskActions";
@@ -15,8 +15,10 @@ import { MaxLengthSelector } from "../components/MaxLengthSelector";
 import { ModeToggle } from "../components/ModeToggle";
 import ResponseArea from "@/components/ResponseArea";
 import { callBot } from "@/lib/llm";
-import { CONTENT_GENERATION_TEMPLATE } from "@/lib/templates";
+import { getTemplate } from "@/lib/templates";
 import { useState } from "react";
+import OpenAIKey from "@/components/OpenAIKey";
+import { ButtonLoading } from "@/components/LoadingButton";
 type Metadata = {
   title: string;
   description: string;
@@ -29,29 +31,66 @@ export const metadata: Metadata = {
 
 const Home = () => {
   const [response, setResponse] = useState<string>("");
+  const [selectedTask, setSelectedTask] = useState<Task>();
+  const [apiKey, setApiKey] = useState<string>("");
   const [selectedModel, setSelectedModel] = useState<Model>(models[0]);
-  const [selectedTemperature, setSelectedTemperature] = useState<number[]>([
-    0.56,
-  ]);
+  const [selectedTemperature, setSelectedTemperature] = useState<number[]>([0]);
   const [selectedMaxLength, setSelectedMaxLength] = useState<number[]>([256]);
   const [query, setQuery] = useState<string>("");
+  const [isSubmitting, setIsSubmitting] = useState<boolean>(false);
 
   const handleSubmit = async () => {
-    const myBot = callBot({
-      systemTemplate: CONTENT_GENERATION_TEMPLATE,
-      modelName: selectedModel.name,
-      temperature: selectedTemperature[0],
-      maxTokens: selectedMaxLength[0],
-    });
-
-    if (query) {
-      const response = await myBot(query);
-      setResponse(response.text);
+    setIsSubmitting(true);
+    if (!apiKey) {
+      alert("Please enter your OpenAI API key");
       return;
     }
 
-    alert("Please enter a query");
+    if (!selectedTask) {
+      alert("Please select a task");
+      return;
+    }
+
+    if (!selectedModel) {
+      alert("Please select a model");
+      return;
+    }
+
+    if (!selectedTemperature) {
+      alert("Please select a temperature");
+      return;
+    }
+
+    if (!selectedMaxLength) {
+      alert("Please select a max length");
+      return;
+    }
+
+    if (!query) {
+      alert("Please enter a query");
+      return;
+    }
+
+    const template = getTemplate(selectedTask);
+
+    const myBot = callBot({
+      systemTemplate: template,
+      modelName: selectedModel.name,
+      temperature: selectedTemperature[0],
+      maxTokens: selectedMaxLength[0],
+      key: apiKey,
+    });
+
+    try {
+      const response = await myBot(query);
+      setResponse(response.text);
+      setIsSubmitting(false);
+    } catch (error: any) {
+      alert(error.response.data.error.message);
+      setIsSubmitting(false);
+    }
   };
+
   return (
     <>
       <div className="md:hidden">
@@ -61,7 +100,11 @@ const Home = () => {
         <div className="container flex flex-col items-start justify-between space-y-2 py-4 sm:flex-row sm:items-center sm:space-y-0 md:h-16">
           <h2 className="hidden md:block text-lg font-semibold">ScribeAI</h2>
           <div className="ml-auto flex w-full space-x-2 sm:justify-end">
-            <TaskSelector tasks={tasks} />
+            <TaskSelector
+              tasks={tasks}
+              setSelectedTask={setSelectedTask}
+              selectedTask={selectedTask as Task}
+            />
             <TaskSave />
             <div className="hidden space-x-2 md:flex">
               <ModeToggle />
@@ -89,6 +132,7 @@ const Home = () => {
                   setSelectedMaxLength={setSelectedMaxLength}
                   selectedMaxLength={selectedMaxLength}
                 />
+                <OpenAIKey setApiKey={setApiKey} apiKey={apiKey} />
               </div>
               <div className="md:order-1">
                 <TabsContent value="complete" className="mt-0 border-0 p-0">
@@ -101,9 +145,13 @@ const Home = () => {
                     />
                     {response && <ResponseArea response={response} />}
                     <div className="flex items-center space-x-2">
-                      <Button onClick={handleSubmit} disabled={!query}>
-                        Submit
-                      </Button>
+                      {isSubmitting ? (
+                        <ButtonLoading />
+                      ) : (
+                        <Button onClick={handleSubmit} disabled={!query}>
+                          Submit
+                        </Button>
+                      )}
                       <Button variant="secondary" disabled={!response}>
                         <span className="sr-only">Show history</span>
                         <CounterClockwiseClockIcon className="h-4 w-4" />
