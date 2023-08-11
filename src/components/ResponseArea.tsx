cc5739646a4385b66017a331f93713af835dc3ac
shehabshalan <shehab.shalan@hotmail.com>
2023-08-11 20:09:56 +0200

    feat: Add markdown support

--- src/components/ResponseArea.tsx
+++ src/components/ResponseArea.tsx
@@ -1,7 +1,11 @@
+import ReactMarkdown from "react-markdown";
+
 const ResponseArea = ({ response }: { response: string }) => {
   return (
     <div className="rounded-md border bg-muted flex-1 p-4 ">
-      <p className="p-4 text-justify">{response}</p>
+      <div className="p-4 text-justify">
+        <ReactMarkdown children={response} />
+      </div>
     </div>
   );
 };
